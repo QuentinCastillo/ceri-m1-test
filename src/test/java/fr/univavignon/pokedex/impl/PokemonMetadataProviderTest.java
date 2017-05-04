36fe908865dfe8769c19096d510570171945f131
Castillo Quentin <quentin.castillo@hotmail.fr>
2017-05-04 18:19:41 +0200

    test IPokemonMetadataProvider

---
+++ src/test/java/fr/univavignon/pokedex/impl/PokemonMetadataProviderTest.java
@@ -0,0 +1,16 @@
+package fr.univavignon.pokedex.impl;
+
+import fr.univavignon.pokedex.api.IPokemonMetadataProviderTest;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+
+
+public class PokemonMetadataProviderTest extends IPokemonMetadataProviderTest {
+    @Before
+    public void setUp() {
+        pokemonMetadataProvider = new PokemonMetadataProvider();
+    }
+
+}
